2c13c84d449aa12ad6406aa367516d509a54a606
Shaik Esafiroz <eshaik@pkrmsezdc.com>
2024-12-16 14:57:14 +0530

    initial commit

--- src/main/java/com/example/inventoryservice/Products.java
+++ src/main/java/com/example/inventoryservice/Products.java
@@ -1,6 +1,9 @@
 package com.example.inventoryservice;
 
 
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +13,12 @@ import lombok.Setter;
 public class Products {
 	private long id;
 	private String product_name;
+	
+	@Nullable
 	private String description;
 	private int price;
+	
+	@Transient
 	private int stock;
 	
 	
